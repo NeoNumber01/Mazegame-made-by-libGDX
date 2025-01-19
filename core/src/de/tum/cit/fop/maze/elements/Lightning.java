767f5da17225246a821ca5c62bba323f09b8c05f
LastiaraZyh <lastiara.zou@tum.de>
2025-01-19 20:30:17 +0100

    feat:implement scaleAdjust

--- core/src/de/tum/cit/fop/maze/elements/Lightning.java
+++ core/src/de/tum/cit/fop/maze/elements/Lightning.java
@@ -6,24 +6,27 @@ import com.badlogic.gdx.math.Vector2;
 public class Lightning extends InteractiveElements {
 
     private final Vector2 position;
-
+    private float scale = 1.0f;
+    private Vector2 offset = new Vector2(0, 0);
     public Lightning(Maze maze, TextureRegion texture, Vector2 position) {
         super(maze, position, new Vector2(32, 32), new Vector2(0, 0));
         this.position = position;
     }
-
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+    public void setOffset(Vector2 offset) {
+        this.offset = offset;
+    }
     @Override
     public void render() {
-        // 确保纹理和位置正确渲染
-        maze.getGame()
-                .getSpriteBatch()
-                .draw(
-                        maze.getGame().getResourcePack().getLightingTexture(),
-                        position.x,
-                        position.y,
-                        maze.getBlockSize(),
-                        maze.getBlockSize());
+        renderTextureV2(
+            maze.getGame().getResourcePack().getLightingTexture(),
+            scale,
+            offset
+        );
     }
+
 
     @Override
     public void onCollision(MazeObject other) {
--- core/src/de/tum/cit/fop/maze/elements/Lives.java
+++ core/src/de/tum/cit/fop/maze/elements/Lives.java
@@ -6,25 +6,26 @@ import com.badlogic.gdx.math.Vector2;
 public class Lives extends InteractiveElements {
     private final Vector2 position;
     private int lives;
-
+    private float scale = 1.0f;
+    private Vector2 offset = new Vector2(0, 0);
     public Lives(Maze maze, TextureRegion texture, Vector2 position) {
         super(maze, position, new Vector2(32, 32), new Vector2(0, 0));
         this.position = position;
     }
-
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+    public void setOffset(Vector2 offset) {
+        this.offset = offset;
+    }
     @Override
     public void render() {
-        // 确保纹理和位置正确渲染
-        maze.getGame()
-                .getSpriteBatch()
-                .draw(
-                        maze.getGame().getResourcePack().getFullHeartTexture(),
-                        position.x,
-                        position.y,
-                        maze.getBlockSize(),
-                        maze.getBlockSize());
+        renderTextureV2(
+            maze.getGame().getResourcePack().getFullHeartTexture(),
+            scale,
+            offset
+        );
     }
-
     @Override
     public void onCollision(MazeObject other) {
         if (other instanceof Player player) {
