0816ab7e7852e8a756c901e3ebed19b9dec8dce8
Shujun Liao <1990403024@qq.com>
2025-01-20 03:06:40 +0100

    continuous injured by trap

--- core/src/de/tum/cit/fop/maze/elements/Trap.java
+++ core/src/de/tum/cit/fop/maze/elements/Trap.java
@@ -1,9 +1,15 @@
 package de.tum.cit.fop.maze.elements;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import de.tum.cit.fop.maze.MazeRunnerGame;
 
 public class Trap extends Block {
+    private static final float DAMAGE_INTERVAL = 0.5f;
+    private float lastDamageTime = 0f;
+    private boolean playerOnTrap = false;
+
     public Trap(Maze maze, TextureRegion texture, Vector2 position) {
         super(maze, texture, position, false);
     }
@@ -12,12 +18,40 @@ public class Trap extends Block {
     public void onArrival(MazeObject other) {
         if (other instanceof Player player) {
             // TODO：播放音效
-
-            player.modifyHealth(-10f);
+            playerOnTrap = true;
 
             Vector2 pos = getPosition();
             int i = (int) ((pos.x - maze.getPosition().x) / maze.getBlockSize());
             int j = (int) ((pos.y - maze.getPosition().y) / maze.getBlockSize());
         }
     }
+
+    @Override
+    public void onFrame(float deltaTime) {
+        if (playerOnTrap) {
+            MazeRunnerGame game = maze.getGame();
+            float currentTime = game.getStateTime();
+            if (currentTime - lastDamageTime >= DAMAGE_INTERVAL) {
+                Player player = maze.getPlayer();
+                if (player != null && this.contains(player.getPosition(),player.getHitbox())) {
+                    player.modifyHealth(-10f); //持续造成伤害
+                    lastDamageTime = currentTime;
+                } else {
+                    playerOnTrap = false; //玩家已离开陷阱
+                }
+            }
+        }
+    }
+    public boolean contains(Vector2 playerPosition, Rectangle playerHitbox) {
+        Rectangle trapRect = new Rectangle(
+            getPosition().x, //陷阱左上角的 x 坐标
+            getPosition().y, //陷阱左上角的 y 坐标
+            maze.getBlockSize(), //陷阱宽
+            maze.getBlockSize()  //陷阱高
+        );
+
+        //检测玩家碰撞盒是否与陷阱碰撞盒重叠
+        return trapRect.overlaps(playerHitbox);
+    }
+
 }
